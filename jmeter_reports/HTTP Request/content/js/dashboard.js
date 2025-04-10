5019c3e6022c668338d185ec4e69469f96ade1a1
Andrei <andrei.bertescu2@gmail.com>
2025-04-10 15:50:48 +0300

    Updated jmeter performance tests

--- jmeter_reports/HTTP Request/content/js/dashboard.js
+++ jmeter_reports/HTTP Request/content/js/dashboard.js
@@ -133,7 +133,7 @@ $(document).ready(function() {
         widgets: ['zebra']
     });
 
-    var data = {"OkPercent": 100.0, "KoPercent": 0.0};
+    var data = {"OkPercent": 94.0, "KoPercent": 6.0};
     var dataset = [
         {
             "label" : "FAIL",
@@ -173,7 +173,7 @@ $(document).ready(function() {
     });
 
     // Creates APDEX table
-    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [1.0, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [1.0, 500, 1500, "Get login page"], "isController": false}, {"data": [1.0, 500, 1500, "Submit login-0"], "isController": false}, {"data": [1.0, 500, 1500, "Submit login"], "isController": false}, {"data": [1.0, 500, 1500, "Submit login-1"], "isController": false}]}, function(index, item){
+    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.94, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [1.0, 500, 1500, "Submit logout-1"], "isController": false}, {"data": [1.0, 500, 1500, "Submit logout-0"], "isController": false}, {"data": [1.0, 500, 1500, "Submit logout"], "isController": false}, {"data": [1.0, 500, 1500, "Get login page"], "isController": false}, {"data": [1.0, 500, 1500, "Create notes"], "isController": false}, {"data": [1.0, 500, 1500, "Create notes-0"], "isController": false}, {"data": [1.0, 500, 1500, "Create notes-1"], "isController": false}, {"data": [1.0, 500, 1500, "Submit login-0"], "isController": false}, {"data": [0.4, 500, 1500, "Submit login"], "isController": false}, {"data": [1.0, 500, 1500, "Submit login-1"], "isController": false}]}, function(index, item){
         switch(index){
             case 0:
                 item = item.toFixed(3);
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 20, 0, 0.0, 69.94999999999999, 3, 222, 62.5, 188.20000000000013, 220.59999999999997, 222.0, 23.923444976076556, 198.869477048445, 7.943331339712919], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Get login page", 5, 0, 0.0, 12.6, 3, 46, 5.0, 46.0, 46.0, 46.0, 6.8119891008174385, 18.114303048365123, 0.8514986376021798], "isController": false}, {"data": ["Submit login-0", 5, 0, 0.0, 110.6, 79, 194, 92.0, 194.0, 194.0, 194.0, 6.55307994757536, 3.334135403014417, 2.758181110747051], "isController": false}, {"data": ["Submit login", 5, 0, 0.0, 133.8, 93, 222, 117.0, 222.0, 222.0, 222.0, 6.377551020408164, 97.55037268813776, 3.8364955357142856], "isController": false}, {"data": ["Submit login-1", 5, 0, 0.0, 22.8, 14, 27, 25.0, 27.0, 27.0, 27.0, 8.488964346349746, 125.52724426994908, 1.533650785229202], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 50, 3, 6.0, 69.0, 2, 404, 33.5, 242.59999999999994, 360.84999999999997, 404.0, 47.52851711026616, 439.16071322480985, 19.503401259505704], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Submit logout-1", 5, 0, 0.0, 6.2, 5, 8, 6.0, 8.0, 8.0, 8.0, 9.900990099009901, 64.82054455445544, 1.7017326732673268], "isController": false}, {"data": ["Submit logout-0", 5, 0, 0.0, 2.6, 2, 3, 3.0, 3.0, 3.0, 3.0, 9.9601593625498, 5.943024775896414, 4.075494895418327], "isController": false}, {"data": ["Submit logout", 5, 0, 0.0, 9.0, 7, 12, 9.0, 12.0, 12.0, 12.0, 9.84251968503937, 70.31057763287401, 5.719042199803149], "isController": false}, {"data": ["Get login page", 5, 0, 0.0, 21.0, 6, 77, 8.0, 77.0, 77.0, 77.0, 6.944444444444444, 18.46652560763889, 0.8680555555555556], "isController": false}, {"data": ["Create notes", 5, 0, 0.0, 60.6, 54, 71, 56.0, 71.0, 71.0, 71.0, 9.057971014492754, 171.63439764492753, 7.306527400362318], "isController": false}, {"data": ["Create notes-0", 5, 0, 0.0, 17.4, 12, 19, 19.0, 19.0, 19.0, 19.0, 10.02004008016032, 2.915988226452906, 5.792835671342686], "isController": false}, {"data": ["Create notes-1", 5, 0, 0.0, 43.0, 35, 53, 37.0, 53.0, 53.0, 53.0, 9.380863039399626, 175.02271927767353, 2.1436737804878048], "isController": false}, {"data": ["Submit login-0", 5, 0, 0.0, 220.8, 109, 355, 203.0, 355.0, 355.0, 355.0, 5.945303210463734, 3.0249052467300834, 2.502368831747919], "isController": false}, {"data": ["Submit login", 5, 3, 60.0, 265.4, 141, 404, 247.0, 404.0, 404.0, 404.0, 5.649717514124294, 106.09110169491525, 3.3986581920903953], "isController": false}, {"data": ["Submit login-1", 5, 0, 0.0, 44.0, 32, 49, 48.0, 49.0, 49.0, 49.0, 9.433962264150942, 172.3522258254717, 1.7043779481132075], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
@@ -217,7 +217,7 @@ $(document).ready(function() {
     }, [[0, 0]], 0, summaryTableHeader);
 
     // Create error table
-    createTable($("#errorsTable"), {"supportsControllersDiscrimination": false, "titles": ["Type of error", "Number of errors", "% in errors", "% in all samples"], "items": []}, function(index, item){
+    createTable($("#errorsTable"), {"supportsControllersDiscrimination": false, "titles": ["Type of error", "Number of errors", "% in errors", "% in all samples"], "items": [{"data": ["The operation lasted too long: It took 247 milliseconds, but should not have lasted longer than 200 milliseconds.", 1, 33.333333333333336, 2.0], "isController": false}, {"data": ["The operation lasted too long: It took 404 milliseconds, but should not have lasted longer than 200 milliseconds.", 1, 33.333333333333336, 2.0], "isController": false}, {"data": ["The operation lasted too long: It took 368 milliseconds, but should not have lasted longer than 200 milliseconds.", 1, 33.333333333333336, 2.0], "isController": false}]}, function(index, item){
         switch(index){
             case 2:
             case 3:
@@ -228,7 +228,7 @@ $(document).ready(function() {
     }, [[1, 1]]);
 
         // Create top5 errors by sampler
-    createTable($("#top5ErrorsBySamplerTable"), {"supportsControllersDiscrimination": false, "overall": {"data": ["Total", 20, 0, "", "", "", "", "", "", "", "", "", ""], "isController": false}, "titles": ["Sample", "#Samples", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors"], "items": [{"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}]}, function(index, item){
+    createTable($("#top5ErrorsBySamplerTable"), {"supportsControllersDiscrimination": false, "overall": {"data": ["Total", 50, 3, "The operation lasted too long: It took 247 milliseconds, but should not have lasted longer than 200 milliseconds.", 1, "The operation lasted too long: It took 404 milliseconds, but should not have lasted longer than 200 milliseconds.", 1, "The operation lasted too long: It took 368 milliseconds, but should not have lasted longer than 200 milliseconds.", 1, "", "", "", ""], "isController": false}, "titles": ["Sample", "#Samples", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors"], "items": [{"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": ["Submit login", 5, 3, "The operation lasted too long: It took 247 milliseconds, but should not have lasted longer than 200 milliseconds.", 1, "The operation lasted too long: It took 404 milliseconds, but should not have lasted longer than 200 milliseconds.", 1, "The operation lasted too long: It took 368 milliseconds, but should not have lasted longer than 200 milliseconds.", 1, "", "", "", ""], "isController": false}, {"data": [], "isController": false}]}, function(index, item){
         return item;
     }, [[0, 0]], 0);
 
